9ae078668f940f93b8dae96569d24d3c958ca351
allen2107 <allen2107@yandex.ru>
2017-06-20 14:52:03 +0300

    webapp test ver.

--- src/com/company/Bootstrap.java
+++ src/com/company/Bootstrap.java
@@ -1,19 +1,25 @@
 package com.company;
 
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
 
 public class Bootstrap {
 
     private static Properties ServerProps = new Properties();
     private static FileInputStream in;
+    static final Logger logger = Logger.getLogger(Bootstrap.class);
 
     public static void main(String[] args) {
         try {
+            PropertyConfigurator.configure("src/logger/log4j.properties");
+            logger.info("Started.");
+
             in = new FileInputStream("config.property");
             ServerProps.load(in);
             int http_port = Integer.parseInt(ServerProps.getProperty("http.port"));
@@ -22,9 +28,20 @@ public class Bootstrap {
 
             Server server = new Server(http_port);
 
-        } catch (IOException e) {
+            WebAppContext context = new WebAppContext();
+            context.setDescriptor("webapp/WEB-INF/web.xml");
+            context.setResourceBase("webapp");
+            context.setContextPath("/");
+            context.setParentLoaderPriority(true);
+
+            server.setHandler(context);
+
+            server.start();
+            server.dumpStdErr();
+            server.join();
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
